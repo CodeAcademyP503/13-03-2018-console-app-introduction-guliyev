a0fe6b034b64787258a77cd5e7bbe80689540b2e
Nicat <stablizator@gmail.com>
2018-03-17 15:09:42 +0400

    Add files via upload

---
+++ ders/ConsoleApp4/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //1 -  Ekrandan girilen ededin nece reqemli oldugunu ekrana yazdiran proqram yazin
+
+
+
+
+            //int reqem;
+            //int number =1;
+            //reqem = Convert.ToInt32(Console.ReadLine());
+
+            //while (reqem > 9)
+            //{
+            //    reqem = reqem / 10;
+            //    number++;
+            //}
+
+            //Console.WriteLine(number);
+
+
+
+
+            //2 - Ekrandan girilen ededin reqemleri cemini ekrana yazdiran program yazin
+
+
+
+            //Console.WriteLine("reqem daxil edin : ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //int totalNumber = 0;
+            //do
+            //{
+            //    totalNumber += number % 10;
+            //    number /= 10;
+
+            //} while (number>0);
+            //Console.WriteLine("Total Number " + totalNumber);
+
+
+
+
+            //3 -  Ekrandan daxil olunan ifadenin daxilinde sait ve samitleri ayri ayri ekrana yazdiran program yazin
+
+
+            //Console.Write("Kelmeni Dxil Edin: ");
+            //string ifade = Console.ReadLine();
+            //string sait = "", samit = "";
+            //ifade = ifade.ToLower();
+            //int sayi = ifade.Length;
+            //for (int i = 0; i <= sayi - 1; i++)
+            //{
+            //    char herf = Convert.ToChar(ifade.Substring(i, 1));
+            //    if (herf == 'a' || herf == 'i' || herf == 'o' || herf == 'u' || herf == 'e')
+            //    {
+            //        sait += herf + "-";
+            //    }
+            //    else
+            //    {
+            //        samit += herf + "-";
+            //    }
+            //}
+            //Console.WriteLine("saitler: " + sait);
+            //Console.WriteLine("samitler: " + samit);
+            //Console.Read();
+
+
+            //4 - Ekrandan daxil olunan cumlenin sozlerini tek tek ekrana cixaran program yazin
+
+            //string cumle;
+            //string[] s;
+            //Console.Write("Cümleyi gir : ");
+            //cumle = Console.ReadLine();
+            //s = cumle.Split(' ');
+            //Console.WriteLine("==================");
+            //foreach (string kelime in s)
+            //{
+            //    Console.WriteLine(kelime);
+            //}
+            //Console.ReadKey();
+
+
+
+
+            //5 ci tapsiriq 
+            //Console.WriteLine("Daxil Edin: ");
+            //string kelme = Console.ReadLine();
+
+            //kelme = kelme.Replace("tenbel", "Loser");
+
+
+
+
+            //Console.WriteLine(kelme);
+
+
+
+
+        }
+    }
+}
